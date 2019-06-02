dd55d9bb5b5b0a2b2ae7b562f37a1cb7d07c4035
siennaballot <siennab323@gmail.com>
2019-06-01 21:07:05 -0700

    DHT with harded coded CIMIS values (test)

--- DHT.py
+++ DHT.py
@@ -5,6 +5,7 @@ import Freenove_DHT as DHT
 thermoPin = 11
 localHumidity = 0
 localTemp = 0
+irrigationTime = 0.0
 sqft = 200          # 200 square feet to be irrigated
 pf = 1.0            # plant factor for lawn
 conversion = 0.62   # constant conversion factor
@@ -13,9 +14,9 @@ systemRate = 17     # 17 gallons per minute = 1020 gallons per hour
 
 def getIrrigationTime():
     # get ET, humidity, and temp from CIMIS
-    cimisHumidity = 
-    cimisTemp = 
-    cimisET = 
+    cimisHumidity = 72
+    cimisTemp = 63.0
+    cimisET = 0.00
 
     # get humidty and temp derating factors 
     # get ET0 using these factors and CIMIS ET
@@ -25,14 +26,14 @@ def getIrrigationTime():
 
     # get gallons of water needed per hour (using gallons needed per day formula divided by 24)
     gallons = ((ET0 * pf * sqft *conversion) / IE) / 24
-    
+    print("Gallons Needed: %f", gallons)
+
     # get time to run irrigation per hour
     irrigationTime = gallons / systemRate
-
+    #print("Irrigation Time: %f", irrigationTime)
 
 def loop():
     dht = DHT.DHT(thermoPin)        # creates DHT class object
-    count = 0
 
     while(True):
         if (localHumidity == 0 and localTemp == 0):
@@ -47,5 +48,8 @@ def loop():
             getIrrigationTime()
             localHumidity = 0
             localTemp = 0
+
+        print("Local Humidity: %f", localHumidity)
+        print("Local Temperature: %f", localTemp)
         
         time.sleep(60)
--- FinalProj.py
+++ FinalProj.py
@@ -6,7 +6,7 @@ import threading
 import RPi.GPIO as GPIO
 import time
 from datetime import datetime
-import Freenove_DHT as DHT
+import DHT
 
 thermoPin = 11  # pin for the thermo sensor
 sensorPin = 13  # pin for the motion sensor
@@ -14,7 +14,20 @@ sensorPin = 13  # pin for the motion sensor
 def setup():
     # setup the board input and output pins
     GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
     GPIO.setup(sensorPin, GPIO.IN)
+
+def loop():
+    t = None
+    print("starting DHT thread")
+    t = threading.Thread(target=DHT.loop)
+    t.daemon = True
+    t.start()
+    # DHT.loop()
+    while(True):
+        if (DHT.irrigationTime != 0):
+            print("Irrigation Time: %f", DHT.irrigationTime)
+        time.sleep(10)
 
 # main function to start program
 if __name__ == '__main__':
