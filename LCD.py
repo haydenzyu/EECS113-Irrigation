65b3253a5a0fb01685095f8fe4cb06cfb0f4f577
Hayden Yu <49046121+haydenzyu@users.noreply.github.com>
2019-06-03 14:36:24 -0700

    Update LCD.py

--- LCD.py
+++ LCD.py
@@ -41,7 +41,7 @@ def display_cimis():#local_temp, local_hum, c_temp, c_hum, local_ET, cimis_ET, w
     sleep(1) #wait for DHT thread to start
     while True:
 #Create strings for the variables
-        print(Relay.output)
+        #print(Relay.output)
         if(Relay.output==False):
             mode = 'On'
         else:
@@ -55,7 +55,7 @@ def display_cimis():#local_temp, local_hum, c_temp, c_hum, local_ET, cimis_ET, w
         cimis_ET_str = 'CIMIS ET:' + str(DHT.cimisET) + ' '
         water_saving_str = 'Water Saved: ' + str(0) + ' '
         addi_water_str = 'Additional Water Used: ' + str(0) + ' '
-        print(DHT.display)
+        #print(DHT.display)
         top_line = relay_str + local_temp_str + local_hum_str #concatenate strings for top line on LCD
         if(DHT.hour>0):
             bot_line = c_temp_str + c_hum_str + local_ET_str + cimis_ET_str + water_saving_str + addi_water_str #concatenate strings for bottom line on LCD
@@ -74,7 +74,6 @@ def display_cimis():#local_temp, local_hum, c_temp, c_hum, local_ET, cimis_ET, w
                 top_line = top_line[1:]+top_line[0]# send first char to last 
                 sleep(0.1)
 #end of create strings
-        #print(DHT.display)
 		
 		
 def destroy():
