05a22d5e1cb3e12c42820e8d429aab3d5bd910bd
haydenzyu <haydenzyu@gmail.com>
2019-06-02 17:11:53 -0700

    senseLED code

---
+++ SenseLED.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+
+ledPin = 12
+sensorPin = 16
+senvar = 0
+
+def setup():
+    print('IR Program is starting...')
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin, GPIO.OUT)
+    GPIO.setup(sensorPin, GPIO.IN)
+
+def loop():
+    while True:
+        if GPIO.input(sensorPin) == GPIO.HIGH:
+            GPIO.output(ledPin, GPIO.HIGH)
+            senvar = 1
+            print(senvar)
+        else:
+            GPIO.output(ledPin, GPIO.LOW)
+            senvar = 0
+            print(senvar)
+
+def destroy():
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+                    
+    
